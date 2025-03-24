bc413d26152efabbef83d1b98ac0d0eac6037ac7
AoiSaki01 <rayhansusanto36@gmail.com>
2025-03-25 02:57:04 +0700

    fix error hydration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,29 @@
+'use client'
 import type React from "react"
-import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import Navbar from "@/components/navbar"
 import { ThemeProvider } from "@/components/theme-provider"
 import { BloodEffect } from "@/components/blood-effect"
+import { useEffect, useState } from "react"
 
 const inter = Inter({ subsets: ["latin"] })
-
-export const metadata: Metadata = {
-  title: "pasifixc coorporation",
-  description: "psfx",
-  generator: 'v0.dev',
-  icons: {
-    icon: '/images/pasifixc.webp'
-  }
-}
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const [isClient, setIsClient] = useState(false)
+
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+
+  if (!isClient) {
+    return null
+  }
+
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className} bg-[#0a0000] text-white min-h-screen`}>
